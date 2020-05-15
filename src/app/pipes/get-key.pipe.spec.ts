a7bc0901c4f4983efc9502dde867dcc434fe421f
rotarrares <rotarrares95@gmail.com>
2020-05-16 00:34:33 +0300

    240 files deleted

--- src/app/pipes/get-key.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetKeyPipe } from './get-key.pipe';
-
-describe('GetKeyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetKeyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/getUser.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetUserPipe } from './getUser.pipe';
-
-describe('GetUserPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetUserPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
